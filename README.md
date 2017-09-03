291ff0b9cb83b5b8c0e77b705a2c9661f5ca915b
JulsRicketti <julsricketti@gmail.com>
2017-09-03 12:01:36 -0400

    chore(README): added more details of the generator in the README file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# generator-contcomp [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+# generator-contcomp [![NPM version][npm-image]][https://www.npmjs.com/package/generator-contcomp] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
 > A simple container-component pattern generator for ReactJS components.
 
 ## Installation
@@ -15,6 +15,50 @@ Then generate your new project:
 ```bash
 yo contcomp
 ```
+
+## Usage instructions:
+
+Simply run the generator, which will proceed to ask for the name of the component.
+
+By default, the generator will create a folder with the chosen component name. In it we will have `component.js`, `container.js` and `index.js` with the recommended default boilerplate code. This code may be changed using the different flags contcomp generator offers. 
+
+```
+├── destinationFolder           # Destination path folder
+    ├── ComponentName           # Generated folder
+    │   ├── component.js        # Generated component file
+    │   ├── container.js        # Generated container file
+    │   ├── index.js            # Generated index file
+```
+
+The generator allows for some flexibility through it's options and arguments. Their details are described in contcomp's help:
+
+`yo contcomp -h`
+
+```Usage:
+  yo contcomp:app [options] [<destination-path>] [<js-system>]
+
+Options:
+  -h,   --help                 # Print the generator's options and usage
+        --skip-cache           # Do not remember prompt answers                                         Default: false
+        --skip-install         # Do not automatically install dependencies                              Default: false
+        --stateless-component  # Will generate a stateless component.                                   Default: false
+        --skip-proptypes       # Will not generate the propTypes.                                       Default: false
+        --skip-index           # Will not generate an index.js file. Just the Component and Container.  Default: false
+        --add-react-methods    # Will generate React's lifecycle methods.                               Default: false
+        --add-redux            # Will generate Redux methods in the container.                          Default: false
+
+Arguments:
+  destination-path  # Changes your generated files destination path.
+Current destination path: components/blah.
+Note: your new destination path will be saved.                                                                                     Type:
+ String  Required: false
+  js-system         # Choose the type of JavaScript module system you would like your generated.
+Current JS System: typescript.
+Available options: ES6, TypeScript and CommonJS.
+Note: your new JavaScript module system will be saved.  Type: String  Required: false
+```
+
+
 
 ## Getting To Know Yeoman
 
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,44 +1,43 @@
-const Generator = require('yeoman-generator')
-const chalk = require('chalk')
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
 
-  constructor (args, opts) {
-    super(args, opts)
-
-    this.argument('destination-path', { 
+    this.argument('destination-path', {
       type: String,
       required: false,
       description: `Changes your generated files destination path. \nCurrent destination path: ${this.config.get('destination-path')}. \n${chalk.bold.red('Note: your new destination path will be saved.')}`,
       value: '/components'
-    })
+    });
     this.argument('js-system', {
       type: String,
       required: false,
       description: `Choose the type of JavaScript module system you would like your generated. \nCurrent JS System: ${this.config.get('js-system')}.\n${chalk.blue('Available options: ES6, TypeScript and CommonJS')}. \n${chalk.bold.red('Note: your new JavaScript module system will be saved.')}`,
       value: 'TypeScript'
-    })
+    });
 
     this.option('stateless-component', {
       description: 'Will generate a stateless component.',
       default: false
-    })
+    });
     this.option('skip-proptypes', {
       description: 'Will not generate the propTypes.',
       default: false
-    })
-    this.option('skip-index', { 
+    });
+    this.option('skip-index', {
       description: 'Will not generate an index.js file. Just the Component and Container.',
       default: false
-    })
+    });
     this.option('add-react-methods', {
       description: 'Will generate React\'s lifecycle methods.',
       default: false
-    })
+    });
     this.option('add-redux', {
       description: 'Will generate Redux methods in the container.',
       default: false
-    })
+    });
   }
 
   prompting() {
@@ -46,60 +45,60 @@ module.exports = class extends Generator {
       type: 'input',
       name: 'componentName',
       message: 'What is the name of your component?'
-    }]
+    }];
 
     return this.prompt(prompts).then(props => {
-      this.props = props
-    })
+      this.props = props;
+    });
   }
 
   writing() {
-    const {componentName} = this.props
-    const availableJS = [ 'typescript', 'commonjs', 'es6' ]
+    const {componentName} = this.props;
+    const availableJS = ['typescript', 'commonjs', 'es6'];
     if (this.options['js-system']) {
       if (availableJS.indexOf(this.options['js-system'].toLowerCase()) !== -1) {
-        this.config.set('js-system', this.options['js-system'])
+        this.config.set('js-system', this.options['js-system']);
       } else {
-        this.log ('ERROR! Unavailable JS system. Please choose one of the following: ES6, TypeScript, CommonJS')
-        this.log (`Current one: ${this.config.get('js-system')}`)
-        return
+        this.log('ERROR! Unavailable JS system. Please choose one of the following: ES6, TypeScript, CommonJS');
+        this.log(`Current one: ${this.config.get('js-system')}`);
+        return;
       }
     }
 
-    if (this.options['destination-path']){
-      this.config.set('destination-path', this.options['destination-path'])
+    if (this.options['destination-path']) {
+      this.config.set('destination-path', this.options['destination-path']);
     }
-    let jsSystem = this.config.get('js-system').toLowerCase()
-    let destinationPath = this.config.get('destination-path').toLowerCase()
+    let jsSystem = this.config.get('js-system').toLowerCase();
+    let destinationPath = this.config.get('destination-path').toLowerCase();
 
-    const hasReactMethods = this.options['add-react-methods']
-    const hasRedux = this.options['add-redux']
-    const skipPropTypes = this.options['skip-proptypes']
-    const useStatelessComponent = this.options['stateless-component']
+    const hasReactMethods = this.options['add-react-methods'];
+    const hasRedux = this.options['add-redux'];
+    const skipPropTypes = this.options['skip-proptypes'];
+    const useStatelessComponent = this.options['stateless-component'];
 
-    const component = useStatelessComponent ? 'stateless-component.js' : 'component.js'
+    const component = useStatelessComponent ? 'stateless-component.js' : 'component.js';
     this.fs.copyTpl(
       this.templatePath(`${jsSystem}/${component}`),
       this.destinationPath(`${destinationPath}/${componentName}/component.js`),
-      { componentName, hasReactMethods, skipPropTypes }
-    )
-    
+      {componentName, hasReactMethods, skipPropTypes}
+    );
+
     this.fs.copyTpl(
       this.templatePath(`${jsSystem}/container.js`),
       this.destinationPath(`${destinationPath}/${componentName}/container.js`),
-      { componentName, hasReactMethods, hasRedux, skipPropTypes }
-    )
+      {componentName, hasReactMethods, hasRedux, skipPropTypes}
+    );
 
     if (!this.options['skip-index']) {
       this.fs.copyTpl(
         this.templatePath(`${jsSystem}/index.js`),
         this.destinationPath(`${destinationPath}/${componentName}/index.js`),
         {componentName}
-      )
+      );
     }
   }
 
-  // install() {
+  // Install() {
   //   this.installDependencies()
   // }
-}
+};
